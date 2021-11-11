a8a2537c27d8b1e5d3b5e96624646693e09d9776
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2021-11-10 21:24:11 -0300

    Criados index.html, index.js e App.js e pasta com imagens

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from './App'
+import ReactDOM  from 'react-dom'
+
+import './Normalize.css'
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
