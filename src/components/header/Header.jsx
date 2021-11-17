f34a4905fdb87f8ff3f79660b43224708c26a69c
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2021-11-17 11:30:40 -0300

    Finalizado responsividade do Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import {FaBars} from 'react-icons/fa'
+
 import Modal from "../modalLogin/Modal";
+
 import "./Header.css";
 
 export default function Header() {
   const [show, setShow] = useState(false);
 
+  const [showLinks, setShowLinks] = useState(false);
 
   return (
       <div className="Navbar">
@@ -14,12 +18,14 @@ export default function Header() {
           </div>
         </div>
         <div className="rightSide">
-          <div className="links">
+          <div className="links" id={showLinks ? "hidden" : ""}>
+            <a href="/">Home</a>
             <a href="paginaDoacao">Doação</a>
             <a href="reservaProdutos">Reservar Produtos</a>
             <a href="cadastro">Cadastro</a>
             <a href="quemSomos">Quem Somos</a>
           </div>
+          <button id="btn-responsivo" onClick={() => setShowLinks(!showLinks)}><FaBars/></button>
           <div className="ModalHeader">
                 <button className="botao-login" onClick={() => setShow(true)}>
                   Login
