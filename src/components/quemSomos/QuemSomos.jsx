23342b673ab1aab55cd6e0385d806288a8258f7e
ricardocossta <rm87404@fiap.com.br>
2021-11-16 22:47:51 -0300

    Responsividade mobile telas Quem Somos/Cadatro/Footer

--- src/components/quemSomos/QuemSomos.jsx
+++ src/components/quemSomos/QuemSomos.jsx
@@ -9,38 +9,62 @@ import Barra from '../img/barraQuemSomos.png'
 export default function QuemSomos() {
   return (
     <div>
-      <Header/>
-        <div className="container-equipe">
-          <h1>NOSSA EQUIPE</h1>
-          <div className="barra-equipe"><img src={Barra} alt="Barra divisória" width="930px" /></div>
-          <div className="equipe">
-            <div className="nome-esq">
-              <div className="dev">
-                <h4>Emanuelle de Fátima - RM88684</h4>
-                <p>github.com/emanuellex</p>
-              </div>
-              <div className="dev">
-                <h4>Ricardo da Silva Costa- RM87404</h4>
-                <p>github.com/ricardocossta</p>
-              </div>
-              <div className="dev">
-                <h4>Yury Oliveira da Silva - RM85595</h4>
-                <p>github.com/yurysilva</p>
-              </div>
+      <Header />
+      <div className="container-equipe">
+        <h1>NOSSA EQUIPE</h1>
+        <div className="barra-equipe">
+          <img src={Barra} alt="Barra divisória" className="img-barra"/>
+        </div>
+        <div className="equipe">
+          <div className="lista-completa">
+            <div className="dev">
+              <h4>Emanuelle de Fátima - RM88684</h4>
+              <p>github.com/emanuellex</p>
             </div>
-            <div className="nome-dir">
-              <div className="dev-dir">
-                <h4>Israel Vieira Ribeiro - RM89347</h4>
-                <p>github.com/isrcode</p>
-              </div>
-              <div className="dev-dir">
-                <h4>Thiago Vazquez Enninger - RM86664</h4>
-                <p>github.com/thiagoenninger</p>
-              </div>
+            <div className="dev">
+              <h4>Ricardo da Silva Costa- RM87404</h4>
+              <p>github.com/ricardocossta</p>
+            </div>
+            <div className="dev">
+              <h4>Yury Oliveira da Silva - RM85595</h4>
+              <p>github.com/yurysilva</p>
+            </div>
+            <div className="dev">
+              <h4>Israel Vieira Ribeiro - RM89347</h4>
+              <p>github.com/isrcode</p>
+            </div>
+            <div className="dev">
+              <h4>Thiago Vazquez Enninger - RM86664</h4>
+              <p>github.com/thiagoenninger</p>
+            </div>
+          </div>
+          <div className="nome-esq">
+            <div className="dev">
+              <h4>Emanuelle de Fátima - RM88684</h4>
+              <p>github.com/emanuellex</p>
+            </div>
+            <div className="dev">
+              <h4>Ricardo da Silva Costa- RM87404</h4>
+              <p>github.com/ricardocossta</p>
+            </div>
+            <div className="dev">
+              <h4>Yury Oliveira da Silva - RM85595</h4>
+              <p>github.com/yurysilva</p>
+            </div>
+          </div>
+          <div className="nome-dir">
+            <div className="dev-dir">
+              <h4>Israel Vieira Ribeiro - RM89347</h4>
+              <p>github.com/isrcode</p>
+            </div>
+            <div className="dev-dir">
+              <h4>Thiago Vazquez Enninger - RM86664</h4>
+              <p>github.com/thiagoenninger</p>
             </div>
           </div>
         </div>
-      <Footer/>
+      </div>
+      <Footer />
     </div>
   )
 }
